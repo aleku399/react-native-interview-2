17d8497d0720bd7995de1f3c33e4d9803eac1a04
aleku399 <aleku399@gmail.com>
2022-03-21 22:07:32 +0300

    Use fetch instead of axios

--- MyPhotos/src/Constants.ts
+++ MyPhotos/src/Constants.ts
@@ -1,14 +1,17 @@
 import {Dimensions, Platform} from 'react-native';
-// import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
+import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
 
 export const CONTENT_SPACING = 15;
 
-const SAFE_BOTTOM = 0;
+const SAFE_BOTTOM =
+  Platform.select({
+    ios: StaticSafeAreaInsets.safeAreaInsetsBottom,
+  }) ?? 0;
 
 export const SAFE_AREA_PADDING = {
-  paddingLeft: CONTENT_SPACING,
-  paddingTop: CONTENT_SPACING,
-  paddingRight: CONTENT_SPACING,
+  paddingLeft: StaticSafeAreaInsets.safeAreaInsetsLeft + CONTENT_SPACING,
+  paddingTop: StaticSafeAreaInsets.safeAreaInsetsTop + CONTENT_SPACING,
+  paddingRight: StaticSafeAreaInsets.safeAreaInsetsRight + CONTENT_SPACING,
   paddingBottom: SAFE_BOTTOM + CONTENT_SPACING,
 };
 
@@ -25,4 +28,4 @@ export const SCREEN_HEIGHT = Platform.select<number>({
 export const CAPTURE_BUTTON_SIZE = 78;
 
 // Add backend API by tunneling on ngrok 4001 (http//localhost:4001)
-export const baseURL = 'https://427e-41-210-146-103.ngrok.io/api';
+export const baseURL = 'https://9ca7-197-239-4-169.ngrok.io/api/photos';
--- MyPhotos/src/lib/apiEndpoints.ts
+++
@@ -1,1 +0,0 @@
-export const photoApi = '/photos';
